e31abd779e2183a010b3fda10a817e1dafb997c1
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2020-11-15 20:05:25 +0100

    Add files via upload

---
+++ vjezba 4/vjezba 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezba_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unos;
+            string sVelika; string sMala; string sPrvaTri; string sZadnjihPet; string s8_11;
+            Console.WriteLine("Molim vas unesite niz znakova!");
+            unos = Console.ReadLine();
+            sVelika = unos.ToUpper();
+            sMala = unos.ToLower();
+            sPrvaTri = unos.Substring(0, 3);
+            sZadnjihPet = unos.Substring(unos.Length - Math.Min(4, unos.Length));
+            s8_11 = unos.Substring(7, 3);
+
+            Console.WriteLine(sVelika);
+            Console.WriteLine(sMala);
+            Console.WriteLine(sPrvaTri);
+            Console.WriteLine(sZadnjihPet);
+            Console.WriteLine(s8_11);
+
+            Console.ReadKey();
+        }
+    }
+}
